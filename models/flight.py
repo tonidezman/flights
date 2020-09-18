3c61a1673ec950d4b6179b94043306e20985edc3
Toni Dezman <toni.dezman@gmail.com>
2020-09-18 08:35:45 +0200

    adds models

---
+++ models/flight.py
@@ -0,0 +1,16 @@
+from app import db
+
+
+class Flight(db.Model):
+    __tablename__ = 'flights'
+    id = db.Column(db.Integer, primary_key=True)
+    flight_number = db.Column(db.String(250))
+    origin_airport = db.Column(db.String(250))
+    destination_airport = db.Column(db.String(250))
+    carrier = db.Column(db.String(250))
+    price = db.Column(db.Integer())
+    day = db.Column(db.String(250))
+    time = db.Column(db.String(250))
+    duration = db.Column(db.String(250))
+    available_seats = db.Column(db.Integer())
+    taken_seats = db.Column(db.Integer(), default=0)

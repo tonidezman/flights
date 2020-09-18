7ecc50caa9887683957a1e50cb4354c5d9b8b01b
Toni Dezman <toni.dezman@gmail.com>
2020-09-18 10:34:06 +0200

    fixes circular dependencie & login works

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash, make_response
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 load_dotenv()
@@ -10,15 +11,35 @@ app.config["SQLALCHEMY_DATABASE_URI"] = f'mysql://{e["DB_USER"]}:{e["DB_PASSWORD
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+from models.user import User  # noqa
+from models.flight import Flight  # noqa
+
 
 @app.route("/")
 def home():
     return "home page"
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'GET':
+        return render_template("login.html")
+    username = request.form['username']
+    user = User.query.filter_by(name=username).first()
+    if user is None:
+        user = User(name=username)
+        db.session.add(user)
+        db.session.commit()
+    res = make_response(redirect("flights"))
+    res.set_cookie("username", username)
+    return res
+
+
+@app.route("/logout")
+def logout():
+    res = make_response(redirect("login"))
+    res.set_cookie("username", "", max_age=0)
+    return res
 
 
 @app.route("/flights")
